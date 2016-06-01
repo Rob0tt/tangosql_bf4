04240de904cf98914f38e02e9f5f675ea546af98
Rob0tt <misterjayx@gmail.com>
2016-06-01 00:13:09 -0400

    Tango
    Setup

--- config/config.php
+++ config/config.php
@@ -6,18 +6,18 @@
 // Read readme.txt for help.
 
 // DATABASE INFORMATION
-DEFINE('HOST', '');													// database host address
+DEFINE('HOST', 'tangosql.tangoworldwide.net');													// database host address
 DEFINE('PORT', '3306');												// database port - default is 3306
-DEFINE('NAME', '');													// database name
-DEFINE('USER', '');													// database user name - sometimes the same as the database name
-DEFINE('PASS', '');													// database password
+DEFINE('NAME', 'tangosql_bf4');													// database name
+DEFINE('USER', 'tangosql_bf4');													// database user name - sometimes the same as the database name
+DEFINE('PASS', 'jEBPz6fysm4xV5Sq');													// database password
 
 // CLAN NAME
-$clan_name		= 'clan';											// your gaming clan or organization name
+$clan_name		= 'TangoWorldWide';											// your gaming clan or organization name
 
 // PAGE BANNER
 $banner_image	= './common/images/bf4-logo.png';					// your desired page banner image
 
 // BANNER LINK
-$banner_url		= 'http://tyger07.github.io/BF4-Server-Stats/';		// where clicking on page banner will take you
+$banner_url		= 'http://tangoworldwide.net/index.php';		// where clicking on page banner will take you
 ?>
